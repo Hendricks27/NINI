8801600bfaddaf438dc66ca2c04403348cf08965
Wanhengz <36709778+Wanhengz@users.noreply.github.com>
2019-03-27 18:52:28 -0400

    week1

---
+++ whzhang/README.md
@@ -0,0 +1,8 @@
+##Week 1
+###Done
+Reference: https://www.kaggle.com/gpreda/lanl-earthquake-eda-and-prediction/notebook
+https://www.kaggle.com/mjbahmani/probability-of-earthquake-eda-fe-5-models/notebook  
+and Yummy's work.
+
+1. Import data 
+   * Data description 
--- whzhang/week1.py
+++ whzhang/week1.py
@@ -1,1 +1,27 @@
-print('hello world')
+import gc
+import os
+import time
+import logging
+import datetime
+import warnings
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import xgboost as xgb
+import lightgbm as lgb
+from scipy import stats
+from scipy.signal import hann
+from tqdm import tqdm_notebook
+import matplotlib.pyplot as plt
+from scipy.signal import hilbert
+from scipy.signal import convolve
+from sklearn.svm import NuSVR, SVR
+from catboost import CatBoostRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import KFold,StratifiedKFold, RepeatedKFold
+warnings.filterwarnings("ignore")
